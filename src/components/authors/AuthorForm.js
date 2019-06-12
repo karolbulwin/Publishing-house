031803792713cb286f4fafc8d7d592a1e1f8f356
Karol Bulwin <karolbulwin@gmail.com>
2019-06-12 22:00:26 +0200

    add author form

---
+++ src/components/authors/AuthorForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TextInput from "../common/TextInput";
+
+const AuthorForm = ({
+  author,
+  onSave,
+  onChange,
+  saving = false,
+  errors = {}
+}) => {
+  return (
+    <div className="container py-4">
+      <form onSubmit={onSave}>
+        <h1 className="py-3">{author.id ? "Edit" : "Add"} Author</h1>
+        {errors.onSave && (
+          <div className="alert alert-danger" role="alert">
+            {errors.onSave}
+          </div>
+        )}
+
+        <TextInput
+          name="name"
+          label="Name"
+          value={author.name}
+          onChange={onChange}
+          error={errors.name}
+        />
+
+        <button
+          type="submit"
+          disabled={saving}
+          className="btn btn-outline-dark btn-lg"
+        >
+          {saving ? "Saving..." : "Save"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+AuthorForm.propTypes = {
+  author: PropTypes.object,
+  errors: PropTypes.object,
+  onSave: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  saving: PropTypes.bool
+};
+
+export default AuthorForm;
