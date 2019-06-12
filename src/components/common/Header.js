7efe9b9866c2de191efd38d71571dc8efb2405b5
Karol Bulwin <karolbulwin@gmail.com>
2019-06-12 19:00:20 +0200

    header

---
+++ src/components/common/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const activeStyle = { color: "black", fontWeight: "bold" };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light py-3">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mr-auto flex-grow-1 justify-content-lg-between">
+          <li className="nav-item">
+            <NavLink
+              className="nav-link"
+              to="/"
+              activeStyle={activeStyle}
+              exact
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/books" activeStyle={activeStyle}>
+              Books
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className="nav-link"
+              to="/authors"
+              activeStyle={activeStyle}
+            >
+              Authors
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about" activeStyle={activeStyle}>
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
