0d68e9dd8068e1ffdff2621cb961bdfbc28ec9ac
Karol Bulwin <karolbulwin@gmail.com>
2019-06-13 11:29:45 +0200

    background

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   const activeStyle = { color: "black", fontWeight: "bold" };
   return (
-    <nav className="navbar navbar-expand-lg navbar-light py-3">
+    <nav className="navbar navbar-expand-lg navbar-light py-3 bg-light">
       <button
         className="navbar-toggler"
         type="button"
