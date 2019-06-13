1f9a4217efd3e1be64618704cf448cc43b62fafe
Karol Bulwin <karolbulwin@gmail.com>
2019-06-13 19:17:40 +0200

    page not found

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageNotFound = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col text-center py-5">
+        <h1 className="py-5">Oops! Page not found.</h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default PageNotFound;
