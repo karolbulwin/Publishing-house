201e437506f8f5459744cc80e2aad8bbad721780
Karol Bulwin <karolbulwin@gmail.com>
2019-06-13 19:15:42 +0200

    about page

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col text-center py-4">
+        <h1>About</h1>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col text-center py-4">
+        <p>
+          This app uses React, Redux, React Router, and many other helpful
+          libraries.
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutPage;
