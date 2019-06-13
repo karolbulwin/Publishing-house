68a6a83fd6917bafe50883fef15e60b535bab369
Karol Bulwin <karolbulwin@gmail.com>
2019-06-13 19:47:42 +0200

    home page

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <div
+      className="container d-flex justify-content-center align-items-center"
+      style={{ height: "80vh" }}
+    >
+      <div className="jumbotron" style={{ background: "rgba(0, 0,0, .03)" }}>
+        <hr className="my-4" />
+        <h1 className="text-center display-4">Publishing house</h1>
+        <hr className="my-4" />
+      </div>
+    </div>
+  </>
+);
+
+export default HomePage;
