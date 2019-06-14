974bde57d5650a3e1425f5d06a649cb2d0473d57
Karol Bulwin <karolbulwin@gmail.com>
2019-06-14 15:55:49 +0200

    app styles

---
+++ src/components/App-styles.js
@@ -0,0 +1,14 @@
+import img from "../narrative-794978_1920.jpg";
+
+const bgImg = {
+  minHeight: "100%",
+  backgroundImage: `url(${img})`,
+  backgroundAttachment: "fixed",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-reapeat",
+  backgroundPosition: "center",
+  paddingBottom: "1em",
+  userSelect: "none"
+};
+
+export default bgImg;
