33b49dd727477f5c1f9f4b6a9645d987b5b3ce6e
Karol Bulwin <karolbulwin@gmail.com>
2019-06-15 10:06:44 +0200

    if img wont load

--- src/components/App-styles.js
+++ src/components/App-styles.js
@@ -3,6 +3,7 @@ import img from "../narrative-794978_1920.jpg";
 const bgImg = {
   minHeight: "100%",
   backgroundImage: `url(${img})`,
+  backgroundColor: "#cccccc",
   backgroundAttachment: "fixed",
   backgroundSize: "cover",
   backgroundRepeat: "no-reapeat",
