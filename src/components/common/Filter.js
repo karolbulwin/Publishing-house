68a99c9163a0b6e1e431fa4bda9a570406cd3e70
Karol Bulwin <karolbulwin@gmail.com>
2019-06-23 18:08:20 +0200

    filter on change

--- src/components/common/Filter.js
+++ src/components/common/Filter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SelectInput from "./SelectInput";
-import SubmitButton from "./SubmitButton";
 
 const Filter = ({ books, authors, onSave, onChange, errors = {} }) => {
   return (
